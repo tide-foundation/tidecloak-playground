c4117fadfbb0b703712b7ceaa2225092e6f7c381
RaymondThach <raymondnthach@gmail.com>
2025-04-24 08:58:02 +1000

    fixed the KcToken delay bug

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
 import "./tailwind.css";
 import { Provider } from "./context/context";
+import Nav from "./components/nav";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Provider>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>TideCloak Demo</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>TideCloak Demo</title>
+          <Nav/>
           {children}
         </Provider>
       </body>
--- app/user/page.js
+++ app/user/page.js
@@ -1,6 +1,20 @@
 "use client"
 
+import IAMService from "../../lib/IAMService";
+import React, { useState, useEffect } from "react";
+
 export default function User(){
+
+    // useEffect(() => {
+    //     IAMService.initIAM(() => {
+    //         if (IAMService.isLoggedIn()){
+                
+    //         }
+            
+    //     });
+    // }, [])
+
+
     return (
         <div><p>hi</p></div>
     )
