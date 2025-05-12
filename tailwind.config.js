e25f66bf0a4aefd97a30ecdb14da38eb8d219420
Dom <dom@tide.org>
2025-05-12 17:52:08 +1000

    updated stuff for codespace to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // VERY important
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
