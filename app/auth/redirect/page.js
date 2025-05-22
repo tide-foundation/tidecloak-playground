3b6b528f227742f9af92467076d983157c039433
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2025-05-22 12:49:29 +1000

    Fix npm run build compilation error

--- app/auth/redirect/page.js
+++ app/auth/redirect/page.js
@@ -10,17 +10,17 @@ import IAMService from "../../../lib/IAMService";
 export default function RedirectPage() {
 
   const {authenticated, contextLoading} = useAppContext();
-  const params = useSearchParams();
-  const auth = params.get("auth");
+  // const params = useSearchParams();
+  // const auth = params.get("auth");
   const router = useRouter();
 
   useEffect(() => {
     if (!contextLoading){
 
-      if (auth === "failed"){
-        sessionStorage.setItem("tokenExpired", true);
-        IAMService.doLogout(); // The redirect would be calling upon itself here to check if authenticated
-      }
+      // if (auth === "failed"){
+      //   sessionStorage.setItem("tokenExpired", true);
+      //   IAMService.doLogout(); // The redirect would be calling upon itself here to check if authenticated
+      // }
 
       if (authenticated){
         router.push("/user");
