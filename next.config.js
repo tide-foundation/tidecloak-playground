bcca289341543f762dc37955a6c15a97b81e82b1
Sasha Le <iamsasha.le@gmail.com>
2025-05-29 18:54:08 +1000

    another config fix

--- next.config.js
+++ next.config.js
@@ -4,4 +4,17 @@
    export default {
     reactStrictMode: false,
     transpilePackages: ['react-icons'],
+    webpack(config, { isServer }) {
+      // 1. Teach webpack to recognise `.mjs`
+      config.resolve.extensions.push('.mjs');
+
+      // 2. Force `.mjs` in node_modules to be parsed as JS
+      config.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      });
+
+      return config;
+    },
   }
